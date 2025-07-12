18b8ef08602259d2e36c4e9f5da6afadda415052
Harshvardhan Patil <100388567+Harsh4949@users.noreply.github.com>
2025-07-12 15:37:54 +0530

    feat: add Tailwind CSS and related plugins
    - Updated package.json to include @tailwindcss/vite as a dev dependency.
    - Updated package-lock.json to reflect the addition of @tailwindcss/vite and its dependencies.
    - Modified vite.config.js to import and use the Tailwind CSS plugin.
    - Updated main.jsx to remove the commented Login component.
    - Updated Login.jsx to comment out unused imports and replace the Lottie animation with a placeholder.

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Login from '../src/pages/Login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <h2>Hello World</h2>
     <App />
-    {/* <Login /> */}
+
   </StrictMode>,
 )
